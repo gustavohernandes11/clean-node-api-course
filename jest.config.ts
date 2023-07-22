405733160a30c09dead11311f3ed6b7dbb6c01c6
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-07-22 14:14:56 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+export default {
+	clearMocks: true,
+	collectCoverage: true,
+	coverageDirectory: "coverage",
+	collectCoverageFrom: ["./src/**.test**"],
+	coveragePathIgnorePatterns: ["\\\\node_modules\\\\"],
+	coverageProvider: "v8",
+	moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
+	testEnvironment: "jest-environment-node",
+	testMatch: [
+		"**/__tests__/**/*.[jt]s?(x)",
+		"**/?(*.)+(spec|test).[tj]s?(x)",
+	],
+	testPathIgnorePatterns: ["\\\\node_modules\\\\"],
+	transform: {
+		"\\.[jt]sx?$": "ts-jest",
+	},
+};
