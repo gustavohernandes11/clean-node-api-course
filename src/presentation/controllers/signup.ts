a39a683a1abcd999cf3ec2e59f67958694ce01ca
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-07-23 14:46:49 -0300

    feat: ensure password and confirmedPassword was provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,15 +1,31 @@
 import { IHttpRequest, IHttpResponse } from "../protocols/http";
 import { MissingParamError } from "../errors/missing-param-error";
+import { InvalidParamError } from "../errors/invalid-param-error";
 import { badRequest } from "../helpers/http-helpers";
+import { IController } from "../protocols/controller";
+import { IEmailValidator } from "../protocols/email-validator";
 
-export class SignUpController {
+export class SignUpController implements IController {
+	private readonly emailValidator: IEmailValidator;
+
+	constructor(emailValidator: IEmailValidator) {
+		this.emailValidator = emailValidator;
+	}
+
 	handle(httpRequest: IHttpRequest): IHttpResponse {
-		const requiredParams = ["name", "email"];
+		const requiredParams = [
+			"name",
+			"email",
+			"password",
+			"confirmedPassword",
+		];
 		for (const param of requiredParams) {
 			if (!httpRequest.body[param]) {
 				return badRequest(new MissingParamError(param));
 			}
 		}
+		const isValid = this.emailValidator.isValid(httpRequest.body.email);
+		if (!isValid) return badRequest(new InvalidParamError("email"));
 
 		return {
 			statusCode: 200,
