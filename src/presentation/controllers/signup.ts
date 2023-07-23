9c69b8d468a0a96dca0bcaf14b9534d7416752ea
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-07-23 16:24:33 -0300

    test: ensure password confirmation

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -30,6 +30,11 @@ export class SignUpController implements IController {
 			const isValid = this.emailValidator.isValid(httpRequest.body.email);
 			if (!isValid) return badRequest(new InvalidParamError("email"));
 
+			if (
+				httpRequest.body.password !== httpRequest.body.confirmedPassword
+			)
+				return badRequest(new InvalidParamError("confirmedPassword"));
+
 			return {
 				statusCode: 200,
 				body: "Ok!",
