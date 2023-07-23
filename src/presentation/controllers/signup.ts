53e8d2033536c2614541fa33b60396b509bb9400
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-07-23 15:53:43 -0300

    feat: ensure error 500 if EmailValidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,7 @@
 import { IHttpRequest, IHttpResponse } from "../protocols/http";
 import { MissingParamError } from "../errors/missing-param-error";
 import { InvalidParamError } from "../errors/invalid-param-error";
-import { badRequest } from "../helpers/http-helpers";
+import { badRequest, serverError } from "../helpers/http-helpers";
 import { IController } from "../protocols/controller";
 import { IEmailValidator } from "../protocols/email-validator";
 
@@ -13,23 +13,27 @@ export class SignUpController implements IController {
 	}
 
 	handle(httpRequest: IHttpRequest): IHttpResponse {
-		const requiredParams = [
-			"name",
-			"email",
-			"password",
-			"confirmedPassword",
-		];
-		for (const param of requiredParams) {
-			if (!httpRequest.body[param]) {
-				return badRequest(new MissingParamError(param));
+		try {
+			const requiredParams = [
+				"name",
+				"email",
+				"password",
+				"confirmedPassword",
+			];
+			for (const param of requiredParams) {
+				if (!httpRequest.body[param]) {
+					return badRequest(new MissingParamError(param));
+				}
 			}
-		}
-		const isValid = this.emailValidator.isValid(httpRequest.body.email);
-		if (!isValid) return badRequest(new InvalidParamError("email"));
+			const isValid = this.emailValidator.isValid(httpRequest.body.email);
+			if (!isValid) return badRequest(new InvalidParamError("email"));
 
-		return {
-			statusCode: 200,
-			body: "Ok!",
-		};
+			return {
+				statusCode: 200,
+				body: "Ok!",
+			};
+		} catch {
+			return serverError();
+		}
 	}
 }
--- src/presentation/helpers/http-helpers.ts
+++ src/presentation/helpers/http-helpers.ts
@@ -1,6 +1,11 @@
+import { ServerError } from "../errors/server-error";
 import { IHttpResponse } from "../protocols/http";
 
 export const badRequest = (httpError: Error): IHttpResponse => ({
 	statusCode: 400,
 	body: httpError,
+});
+export const serverError = (): IHttpResponse => ({
+	statusCode: 500,
+	body: new ServerError(),
 });
