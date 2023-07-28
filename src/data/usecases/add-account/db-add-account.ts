c9ef31ad1a0554b991b49fafbc930866d72c304a
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-07-28 01:50:16 -0300

    feat: ensure addAccountRepository have been called in DbAddAccount

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -2,18 +2,27 @@ import {
 	IAccountModel,
 	IAddAccount,
 	IAddAccountModel,
+	IAddAccountRepository,
 	IEncrypter,
 } from "./db-add-account-protocols";
 
 export class DbAddAccount implements IAddAccount {
 	private readonly encrypter;
-	constructor(encrypter: IEncrypter) {
+	private readonly addAccountRepository;
+	constructor(
+		encrypter: IEncrypter,
+		addAccountRepository: IAddAccountRepository
+	) {
 		this.encrypter = encrypter;
+		this.addAccountRepository = addAccountRepository;
 	}
-	async add(account: IAddAccountModel): Promise<IAccountModel> {
-		await this.encrypter.encrypt(account.password);
-		return new Promise((resolve) =>
-			resolve(null as unknown as IAccountModel)
+	async add(accountData: IAddAccountModel): Promise<IAccountModel> {
+		const hashedPassword = await this.encrypter.encrypt(
+			accountData.password
 		);
+		const account = await this.addAccountRepository.add(
+			Object.assign({}, accountData, { password: hashedPassword })
+		);
+		return account;
 	}
 }
