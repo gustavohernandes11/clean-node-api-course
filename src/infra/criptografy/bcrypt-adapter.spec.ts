9875ee68ed921e2b4826485526693c4660262314
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-07-31 15:44:56 -0300

    test: ensure a encrypted return from bcrypt-adapter

--- src/infra/criptografy/bcrypt-adapter.spec.ts
+++ src/infra/criptografy/bcrypt-adapter.spec.ts
@@ -6,14 +6,26 @@ jest.mock("bcrypt", () => ({
 	},
 }));
 describe("Bcrypt Adapter", () => {
-	it("should call encrypt with the correct value", () => {
-		const salt = 10;
-		const sut = new BcryptAdaper(salt);
-		const hashSpy = jest.spyOn(sut, "encrypt");
-		const target = "some_value";
+	describe("encrypt", () => {
+		it("should call encrypt with the correct value", () => {
+			const salt = 10;
+			const sut = new BcryptAdaper(salt);
+			const hashSpy = jest.spyOn(sut, "encrypt");
+			const target = "some_value";
 
-		sut.encrypt(target);
+			sut.encrypt(target);
 
-		expect(hashSpy).toHaveBeenCalledWith(target);
+			expect(hashSpy).toHaveBeenCalledWith(target);
+		});
+		it("should return a encrypted value", async () => {
+			const salt = 10;
+			const sut = new BcryptAdaper(salt);
+
+			const original = "some_value";
+			const retuned = await sut.encrypt(original);
+
+			expect(retuned).toBe("HASHED_VALUE");
+			expect(retuned).not.toBe(original);
+		});
 	});
 });
