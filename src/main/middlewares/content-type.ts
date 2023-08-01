ec3fed778eb78967e64c3467b0c5f06fc9b0ea22
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-08-01 15:13:14 -0300

    refactor: unify the middlewares in one file to export

--- src/main/middlewares/content-type.ts
+++ src/main/middlewares/content-type.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
-const contentType = (_: Request, res: Response, next: NextFunction) => {
+export const contentType = (_: Request, res: Response, next: NextFunction) => {
 	res.type("json");
 	next();
 };
-export default contentType;
--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -1,9 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 
-const cors = (_: Request, res: Response, next: NextFunction) => {
+export const cors = (_: Request, res: Response, next: NextFunction) => {
 	res.set("access-control-allow-origin", "*");
 	res.set("access-control-allow-headers", "*");
 	res.set("access-control-allow-methods", "*");
 	next();
 };
-export default cors;
