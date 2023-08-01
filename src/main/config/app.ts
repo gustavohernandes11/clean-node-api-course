3783a77bc9cbe7a4ccad0e1f725dc223477c350e
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-08-01 15:02:21 -0300

    chore: dynamically add routes with setupRoutes

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,8 @@
 import server from "express";
 import setupMiddlawares from "./middlewares";
+import setupRoutes from "./routes";
 
 const app = server();
 setupMiddlawares(app);
+setupRoutes(app);
 export default app;
