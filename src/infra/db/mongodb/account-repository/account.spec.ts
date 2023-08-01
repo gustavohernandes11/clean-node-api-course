093ffd20444f70bf51076f00fe365089c7e27e52
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-08-01 14:42:34 -0300

    test: ensure mongodb return an account on sucess

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -2,13 +2,31 @@ import { MongoHelper } from "../helpers/mongo-helper";
 import { AccountMongoRepository } from "./account";
 import { Collection } from "mongodb";
 
+let accountCollection: Collection;
 describe("Account Mongo Repository", () => {
-	it("should return an account on sucess", () => {
-		/*
-		To resolve:
-		
-		Error: Status Code is 403 (MongoDB's 404)
-		This means that the requested version-platform combination doesn't exist
-		*/
+	beforeAll(async () => {
+		await MongoHelper.connect("mongodb://localhost:27017");
+	});
+
+	afterAll(async () => {
+		await MongoHelper.disconnect();
+	});
+
+	beforeEach(async () => {
+		accountCollection = MongoHelper.getCollection("accounts");
+		await accountCollection.deleteMany({});
+	});
+	it("should return an account on sucess", async () => {
+		const sut = new AccountMongoRepository();
+		const account = await sut.add({
+			email: "tal",
+			name: "tal",
+			password: "asdf",
+		});
+		expect(account).toBeTruthy;
+		expect(account.id).toBeTruthy;
+		expect(account.name).toBe("tal");
+		expect(account.email).toBe("tal");
+		expect(account.password).toBe("asdf");
 	});
 });
