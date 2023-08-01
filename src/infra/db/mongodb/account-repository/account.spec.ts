4c3a8cad4ecc84cbe9e68ec3e8f5151ab583a310
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-08-01 10:22:00 -0300

    test: UNRESOLVED test to mongodb

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,14 @@
+import { MongoHelper } from "../helpers/mongo-helper";
+import { AccountMongoRepository } from "./account";
+import { Collection } from "mongodb";
+
+describe("Account Mongo Repository", () => {
+	it("should return an account on sucess", () => {
+		/*
+		To resolve:
+		
+		Error: Status Code is 403 (MongoDB's 404)
+		This means that the requested version-platform combination doesn't exist
+		*/
+	});
+});
