da7807ab6f3e135d884c7ee6911743e2654f6d4c
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-08-03 16:24:14 -0300

    feat: add log repository to controller factory

--- src/main/factories/signup-controller-factory.ts
+++ src/main/factories/signup-controller-factory.ts
@@ -3,7 +3,8 @@ import { SignUpController } from "../../presentation/controllers/signup/signup";
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account";
 import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
 import { BcryptAdaper } from "../../infra/criptografy/bcrypt-adapter";
-import { LogControllerDecorator } from "main/decorators/log";
+import { LogControllerDecorator } from "../../main/decorators/log";
+import { LogMongoRepository } from "../../infra/db/mongodb/log-repository/log";
 
 export const makeSignUpController = () => {
 	const salt = 12;
@@ -18,5 +19,6 @@ export const makeSignUpController = () => {
 		emailValidatorAdaper,
 		dbAddAccount
 	);
-	return new LogControllerDecorator(signUpController);
+	const logMongoRepository = new LogMongoRepository();
+	return new LogControllerDecorator(signUpController, logMongoRepository);
 };
