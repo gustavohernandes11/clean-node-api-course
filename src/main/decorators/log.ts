4f078d0573a02d7b8ff48daa2d2414d66a8a46cd
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-08-03 11:50:00 -0300

    feat: add logErrorRepository to decorator

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,3 +1,4 @@
+import { ILogErrorRepository } from "data/protocols/log-error-repository";
 import {
 	IController,
 	IHttpRequest,
@@ -5,10 +6,14 @@ import {
 } from "presentation/protocols";
 
 export class LogControllerDecorator implements IController {
-	constructor(private readonly controller: IController) {}
+	constructor(
+		private readonly controller: IController,
+		private readonly logErrorRepository: ILogErrorRepository
+	) {}
 	async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
 		const response = await this.controller.handle(httpRequest);
 		if (response.statusCode === 500) {
+			this.logErrorRepository.log(response.body.stack);
 		}
 		return response;
 	}
