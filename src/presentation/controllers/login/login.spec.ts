0157c1af4bdfe64b22d1b1253c2673fd4d456d9b
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-08-04 20:05:31 -0300

    test: ensure email validator verify the correct email

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -20,13 +20,26 @@ import {
 import { LoginController } from "./login";
 
 describe("LoginController", () => {
+	function makeEmailValidator(): IEmailValidator {
+		class EmailValidatorStub implements IEmailValidator {
+			isValid(): boolean {
+				return true;
+			}
+		}
+
+		return new EmailValidatorStub();
+	}
+
 	type SuiType = {
 		sut: IController;
+		emailValidatorStub: IEmailValidator;
 	};
 
 	function makeSut(): SuiType {
+		const emailValidatorStub = makeEmailValidator();
 		return {
-			sut: new LoginController(),
+			sut: new LoginController(emailValidatorStub),
+			emailValidatorStub,
 		};
 	}
 
@@ -43,7 +56,7 @@ describe("LoginController", () => {
 			badRequest(new MissingParamError("email"))
 		);
 	});
-	it("should return 400 if password was not provided", async () => {
+	it("should return 400 if password is not provided", async () => {
 		const { sut } = makeSut();
 		const httpRequest = {
 			body: {
@@ -55,5 +68,32 @@ describe("LoginController", () => {
 		expect(httpResponse).toEqual(
 			badRequest(new MissingParamError("password"))
 		);
+	});
+	it("should return 400 if invalid email is provided", async () => {
+		const { sut, emailValidatorStub } = makeSut();
+		jest.spyOn(emailValidatorStub, "isValid").mockReturnValueOnce(false);
+		const httpRequest = {
+			body: {
+				email: "invalid_email",
+				password: "123",
+			},
+		};
+		const httpResponse = await sut.handle(httpRequest);
+		expect(httpResponse.statusCode).toBe(400);
+		expect(httpResponse).toEqual(
+			badRequest(new InvalidParamError("email"))
+		);
+	});
+	it("should verify the correct email with emailValidator", async () => {
+		const { sut, emailValidatorStub } = makeSut();
+		const isValidSpy = jest.spyOn(emailValidatorStub, "isValid");
+		const httpRequest = {
+			body: {
+				email: "jhondoe@gmail.com",
+				password: "123",
+			},
+		};
+		await sut.handle(httpRequest);
+		expect(isValidSpy).toHaveBeenCalledWith("jhondoe@gmail.com");
 	});
 });
