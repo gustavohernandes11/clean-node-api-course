3a73c62bff9d150d4582aa3c662cb733ae523a60
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-08-06 19:56:53 -0300

    test: ensure validator composite returns an error

---
+++ src/presentation/validation/validators/validator-composite.spec.ts
@@ -0,0 +1,35 @@
+import { InvalidParamError } from "../../../presentation/errors";
+import { IValidation } from "../../protocols/validation";
+import { ValidatorComposite } from "./validator-composite";
+
+class ValidationSpy implements IValidation {
+	validate(_: any) {
+		return null;
+	}
+}
+type SutType = {
+	sut: IValidation;
+	validations: IValidation[];
+};
+const makeSut = (): SutType => {
+	const validations: IValidation[] = [
+		new ValidationSpy(),
+		new ValidationSpy(),
+	];
+
+	return {
+		sut: new ValidatorComposite(validations),
+		validations,
+	};
+};
+describe("Validator Composite", () => {
+	it("should return an error if any validation fails", async () => {
+		const { sut, validations } = makeSut();
+		jest.spyOn(validations[0], "validate").mockReturnValueOnce(
+			new InvalidParamError("exemple_param")
+		);
+		jest.spyOn(validations[1], "validate").mockReturnValueOnce(new Error());
+		const error = await sut.validate({ randomParam: "" });
+		expect(error).toEqual(new InvalidParamError("exemple_param"));
+	});
+});
